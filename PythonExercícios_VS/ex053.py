c2a04231cd8fe142d3dac619adf54f363c74d598
Renata Santos <renatados.santosp@gmail.com>
2021-02-17 20:13:41 -0300

    Update ex053.py
    Com comentário.

--- PythonExercícios_VS/ex053.py
+++ PythonExercícios_VS/ex053.py
@@ -1,6 +1,9 @@
 from time import sleep
 print(('=')*10, 'PALÍNDROMO', ('=')*10)
 usuario = input('Digite uma frase: ').replace(' ', '').upper()
+'''ao invés disso, poderia ter simplesmente colocado:
+invertido = usuario[::-1] que ele iria inverter...
+usei o "for" por ser este o objetivo da aula'''
 invertido = ''
 for letra in range(len(usuario) - 1, -1, -1):
     invertido = invertido + usuario[letra]
