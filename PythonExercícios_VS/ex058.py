bf35268f062827bd77e12002234d71f4b3d998c4
Renata Santos <renatados.santosp@gmail.com>
2021-11-23 13:10:11 -0300

    cursoemvideo

--- PythonExercícios_VS/ex058.py
+++ PythonExercícios_VS/ex058.py
@@ -7,7 +7,7 @@ print('Olá! Sou seu computador e vou pensar em um número entre 0 e 10.')
 sleep(1)
 print('Pensando...')
 sleep(2)
-pc = randint(1, 11)
+pc = randint(1, 10)
 print('Pensei! Será que você consegue adivinhar qual número foi?')
 palpite = int(input('\nQual é o seu palpite? '))
 
@@ -17,8 +17,12 @@ if palpite == pc:
     print('\nUauu.. Você é um gênio da legilimência! Acertou na primeira tentativa!')
 else:
     while palpite != pc:
-        palpite = int(input('Uuuuh.. você errou, amigo. Dê um novo palpite: '))
-        n += 1
+        if palpite > pc:
+            palpite = int(input('Uuuuh.. você errou, colega. Dê um palpite MENOR: '))
+            n += 1
+        else:
+            palpite = int(input('Uuuuh.. você errou, colega. Dê um palpite MAIOR: '))
+            n += 1
     print('\nUauu! Você sabe mesmo ler mentes! Acertou em {} tentativas.'.format(n))
 
 '''Quero fazer de um jeito que vai dar para o abençoado que tá jogando decidir se quer jogar novamente ou não
